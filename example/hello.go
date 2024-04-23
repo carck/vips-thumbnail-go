a17236a913196f1df90e2ccec4e9caba30314d4a
lance.zhou <lance.zhou@ringcentral.com>
2024-04-23 16:53:11 +0800

    cgo

--- example/hello.go
+++ example/hello.go
@@ -8,5 +8,8 @@ func main() {
 	vips.Start()
 	defer vips.Shutdown()
 
-	vips.Thumbnail("./abc.jpg", "123.jpg", 224, 224, 0, "srgb")
+	err := vips.Thumbnail("./abc.jpg", "123.jpg[Q=90,optimize_coding,keep=none,subsample-mode=on]", 224, 224, 0, "srgb")
+	if err != nil {
+		panic(err)
+	}
 }
