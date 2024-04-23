10a5a11addcf6698a976f5ce3b376c4bc83104b1
lance.zhou <lance.zhou@ringcentral.com>
2024-04-23 16:32:36 +0800

    cgo

--- vips.h
+++ vips.h
@@ -1,4 +1,10 @@
+#ifndef VIPS_THUMBNAIL_H
+
+#define VIPS_THUMBNAIL_H
+
 #include <stdlib.h>
 #include <vips/vips.h>
 
 int thumbnail(const char *filename, const char *outputname, int width, int height, int crop, const char* export_profile);
+
+#endif
